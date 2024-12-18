d59e2280f4d610691a36e09c8fc0a5bd0abdb69f
guyonstephane <guyonst@free.fr>
2024-12-18 22:02:41 +0100

    first commit

---
+++ app/Http/Controllers/MoviesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class MoviesController extends Controller
+{
+    
+
+    public function index() {
+
+        $popularMovies = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/popular')
+            ->json()['results'];
+
+        dd($popularMovies) ;
+            
+        
+        return view('index');
+    }
+}
---
+++ services.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+
+    'tmdb' => [
+        'token' => env('TMDB_TOKEN'),
+    
+    ],
+];
+
+?>
